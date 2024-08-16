c8587a5e68bc7412d777a6752e86053e0fc9466f
pierbotteroweb <pierbotteroweb@gmail.com>
2024-08-16 12:39:46 -0300

    test

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
@@ -21,5 +20,19 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the navbar brand', () => {
+    const navbarBrand = fixture.nativeElement.querySelector('.navbar-brand');
+    expect(navbarBrand.textContent).toContain('Shuffle TV');
+  });
+
+  it('should render the navbar links', () => {
+    const navbarLinks = fixture.nativeElement.querySelectorAll('.nav-link');
+    expect(navbarLinks.length).toBe(4);
+    expect(navbarLinks[0].textContent).toContain('Video');
+    expect(navbarLinks[1].textContent).toContain('Painel');
+    expect(navbarLinks[2].textContent).toContain('Lista');
+    expect(navbarLinks[3].textContent).toContain('Playlist');
   });
 });
