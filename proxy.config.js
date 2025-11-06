3a19c751796104f86322b63512cba83d5e368492
Pier Bottero <pierbotteroweb@gmail.com>
2025-11-05 22:45:19 -0300

    chore(proxy) - angular proxy updated after docker project integration

--- proxy.config.js
+++ proxy.config.js
@@ -1,8 +1,20 @@
-const proxy = [
-    {
-      context: '/api',
-      target: 'http://thisisshuffletv:5091',
-      pathRewrite: {'^/api' : ''}
-    }
-  ];
-  module.exports = proxy;
+const PROXY_CONFIG = [
+  {
+    context: ['/api'],
+    target: 'http://file-server:5091',
+    secure: false,
+    changeOrigin: true,
+    pathRewrite: { '^/api': '' },
+    logLevel: 'debug'
+  },
+  {
+    context: ['/backend'],
+    target: 'http://backend-server:9091',
+    secure: false,
+    changeOrigin: true,
+    pathRewrite: { '^/backend': '' },
+    logLevel: 'debug'
+  }
+];
+
+module.exports = PROXY_CONFIG;
