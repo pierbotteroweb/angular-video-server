af483436404d2743be54c2a7bb956131ec7dff37
Pier Bottero <pierbotteroweb@gmail.com>
2025-12-13 21:10:18 -0300

    feature - new sub components

---
+++ src/app/arquivos/components/files-management/files-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-files-management',
+  templateUrl: './files-management.component.html',
+  styleUrls: ['./files-management.component.scss']
+})
+export class FilesManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
