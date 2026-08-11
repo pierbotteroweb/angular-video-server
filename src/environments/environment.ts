5edd86c693bdea2fd54743df76ccf633df82cf21
Pier Bottero <pierbotteroweb@gmail.com>
2026-08-10 23:42:39 -0300

    rota para nest server atualizada

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   BASE_URL:"http://thisisshuffletv/files/assets/dvds/",
-  API: 'http://localhost:3000/',
+  API: 'http://thisisshuffletv/api-nest/',
   firebase: {
     apiKey: "AIzaSyCwnQT7_QChY_ce7z3uoiFWy6GMqlF--1c",
     authDomain: "shuffletv-dec45.firebaseapp.com",
